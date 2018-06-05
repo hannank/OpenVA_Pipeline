ff9b9a81b737e11c6cca0e40ffb0f1a3ecf3ea08
jarathomas <jarathomas@gmail.com>
2018-06-05 18:36:35 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ pygments_style = 'sphinx'
 #
 #html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
+#html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
